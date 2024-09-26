678d4afd165a6a8c130786098e40ca7fd3dd93f0
Jiří H. <jiri@bacehub.de>
2024-09-26 20:16:05 +0200

    draw KinematicSensor gizmos

--- src/dev_tools.rs
+++ src/dev_tools.rs
@@ -2,6 +2,7 @@
 
 use crate::prelude::*;
 use bevy::{
+    color::palettes::tailwind,
     dev_tools::{states::log_transitions, ui_debug_overlay::DebugUiPlugin},
     input::common_conditions::input_toggle_active,
 };
@@ -18,10 +19,13 @@ pub(super) fn plugin(app: &mut App) {
         );
 
     #[cfg(feature = "dev")]
-    app.add_plugins((
-        WorldInspectorPlugin::new().run_if(input_toggle_active(false, MouseButton::Right)),
-        avian2d::debug_render::PhysicsDebugPlugin::default(),
-    ));
+    {
+        app.add_plugins((
+            WorldInspectorPlugin::new().run_if(input_toggle_active(false, MouseButton::Right)),
+            avian2d::debug_render::PhysicsDebugPlugin::default(),
+        ));
+        app.add_systems(FixedUpdate, draw_kinematic_sensor_gizmos);
+    }
 }
 
 fn draw_level_grid(mut gizmos: Gizmos) {
@@ -35,4 +39,22 @@ fn draw_level_grid(mut gizmos: Gizmos) {
             LinearRgba::gray(0.1),
         )
         .outer_edges();
+}
+
+fn draw_kinematic_sensor_gizmos(
+    sensor_q: Query<(&KinematicSensor, &Transform, Has<Grounded>)>,
+    mut gizmos: Gizmos,
+) {
+    for (sensor, t, is_grounded) in &sensor_q {
+        gizmos.rect_2d(
+            sensor.translation(t.translation),
+            0.,
+            sensor.size,
+            if is_grounded {
+                tailwind::BLUE_400
+            } else {
+                tailwind::GREEN_400
+            },
+        );
+    }
 }
