dd5b4b26af978af887b01e224c27c2761bc0493f
meepleek <ergocrab@gmail.com>
2025-10-21 01:05:35 +0200

    chore: update credits UI

--- src/screens/credits.rs
+++ src/screens/credits.rs
@@ -9,20 +9,19 @@ pub(super) fn plugin(app: &mut App) {
 }
 
 fn show_credits_screen(mut commands: Commands) {
-    commands
-        .ui_root()
-        .insert(DespawnOnExit(Screen::Credits))
-        .with_children(|children| {
-            children.header("Made by");
-            children.label("Joe Shmoe - Implemented aligator wrestling AI");
-            children.label("Jane Doe - Made the music for the alien invasion");
+    commands.spawn((ui_root("credits"),
+        DespawnOnExit(Screen::Credits),
+        children![
+            header("Made by"),
+            label("Joe Shmoe - Implemented aligator wrestling AI"),
+            label("Jane Doe - Made the music for the alien invasion"),
 
-            children.header("Assets");
-            children.label("Bevy logo - All rights reserved by the Bevy Foundation. Permission granted for splash screen use when unmodified.");
-            children.label("Ducky sprite - CC0 by Caz Creates Games");
-            children.label("Button SFX - CC0 by Jaszunio15");
-            children.label("Music - CC BY 3.0 by Kevin MacLeod");
+            header("Assets"),
+            label("Bevy logo - All rights reserved by the Bevy Foundation. Permission granted for splash screen use when unmodified."),
+            label("Ducky sprite - CC0 by Caz Creates Games"),
+            label("Button SFX - CC0 by Jaszunio15"),
+            label("Music - CC BY 3.0 by Kevin MacLeod"),
 
-            children.button("Back").observe(trigger_transition_to_main_menu);
-        });
+            button("Back", trigger_transition_to_main_menu),
+        ]));
 }
