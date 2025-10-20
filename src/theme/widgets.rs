02ee95a8ba4b203e1a41f71984e08b82d62f59d2
meepleek <ergocrab@gmail.com>
2025-10-20 19:31:37 +0200

    chore: update widgets to use current bevy_new_2d approach

--- src/theme/widgets.rs
+++ src/theme/widgets.rs
@@ -1,167 +1,158 @@
-//! Helper traits for creating common widgets.
+//! Helper functions for creating common widgets.
 
-use super::{interaction::InteractionPalette, palette::*};
-use crate::prelude::*;
-use bevy::{ecs::system::EntityCommands, ui::Val::*};
+use std::borrow::Cow;
+
+use bevy::{
+    ecs::{spawn::SpawnWith, system::IntoObserverSystem},
+    prelude::*,
+};
+
+use crate::theme::{interaction::InteractionPalette, palette::*};
 
 pub(crate) const BUTTON_WIDTH: f32 = 200.0;
 pub(crate) const BUTTON_HEIGHT: f32 = 65.0;
 
-/// An extension trait for spawning UI widgets.
-pub trait Widgets {
-    /// Spawn a simple button with text.
-    fn button(&mut self, text: impl Into<String>) -> EntityCommands;
-
-    /// Spawn a simple header label. Bigger than [`Widgets::label`].
-    fn header(&mut self, text: impl Into<String>) -> EntityCommands;
-
-    /// Spawn a simple text label.
-    fn label(&mut self, text: impl Into<String>) -> EntityCommands;
+/// A root UI node that fills the window and centers its content.
+pub fn ui_root(name: impl Into<Cow<'static, str>>) -> impl Bundle {
+    (
+        Name::new(name),
+        Node {
+            position_type: PositionType::Absolute,
+            width: percent(100),
+            height: percent(100),
+            align_items: AlignItems::Center,
+            justify_content: JustifyContent::Center,
+            flex_direction: FlexDirection::Column,
+            row_gap: px(20),
+            ..default()
+        },
+        // Don't block picking events for other UI roots.
+        Pickable::IGNORE,
+    )
 }
 
-impl<T: Spawn> Widgets for T {
-    fn button(&mut self, text: impl Into<String>) -> EntityCommands {
-        let mut entity = self.spawn((
-            Name::new("Button"),
-            ButtonBundle {
-                style: Style {
-                    width: Px(BUTTON_WIDTH),
-                    height: Px(BUTTON_HEIGHT),
-                    justify_content: JustifyContent::Center,
-                    align_items: AlignItems::Center,
-                    ..default()
-                },
-                background_color: BackgroundColor(NODE_BACKGROUND),
+/// A simple header label. Bigger than [`label`].
+pub fn header(text: impl Into<String>) -> impl Bundle {
+    (
+        Name::new("Header"),
+        Node {
+            width: px(500.0),
+            height: px(65.0),
+            justify_content: JustifyContent::Center,
+            align_items: AlignItems::Center,
+            ..default()
+        },
+        Text(text.into()),
+        TextFont::from_font_size(40.0),
+        TextColor(HEADER_TEXT),
+    )
+}
+
+/// A simple text label.
+pub fn label(text: impl Into<String>) -> impl Bundle {
+    (
+        Name::new("Label"),
+        Node {
+            width: px(500.0),
+            height: px(65.0),
+            justify_content: JustifyContent::Center,
+            align_items: AlignItems::Center,
+            ..default()
+        },
+        Text(text.into()),
+        TextFont::from_font_size(24.0),
+        TextColor(LABEL_TEXT),
+    )
+}
+
+/// A large rounded button with text and an action defined as an [`Observer`].
+pub fn button<E, B, M, I>(text: impl Into<String>, action: I) -> impl Bundle
+where
+    E: EntityEvent,
+    B: Bundle,
+    I: IntoObserverSystem<E, B, M>,
+{
+    button_base(
+        text,
+        action,
+        (
+            Node {
+                width: px(BUTTON_WIDTH),
+                height: px(BUTTON_HEIGHT),
+                align_items: AlignItems::Center,
+                justify_content: JustifyContent::Center,
                 ..default()
             },
-            InteractionPalette {
-                none: NODE_BACKGROUND,
-                hovered: BUTTON_HOVERED_BACKGROUND,
-                pressed: BUTTON_PRESSED_BACKGROUND,
-            },
-        ));
-        entity.with_children(|children| {
-            children.spawn((
-                Name::new("Button Text"),
-                Text::from_section(
-                    text,
-                    TextStyle {
-                        font_size: 40.0,
-                        color: BUTTON_TEXT,
-                        ..default()
+            BorderRadius::MAX,
+        ),
+    )
+}
+
+/// A small square button with text and an action defined as an [`Observer`].
+pub fn button_small<E, B, M, I>(text: impl Into<String>, action: I) -> impl Bundle
+where
+    E: EntityEvent,
+    B: Bundle,
+    I: IntoObserverSystem<E, B, M>,
+{
+    button_base(
+        text,
+        action,
+        Node {
+            width: px(30),
+            height: px(30),
+            align_items: AlignItems::Center,
+            justify_content: JustifyContent::Center,
+            ..default()
+        },
+    )
+}
+
+/// A simple button with text and an action defined as an [`Observer`]. The button's layout is provided by `button_bundle`.
+fn button_base<E, B, M, I>(
+    text: impl Into<String>,
+    action: I,
+    button_bundle: impl Bundle,
+) -> impl Bundle
+where
+    E: EntityEvent,
+    B: Bundle,
+    I: IntoObserverSystem<E, B, M>,
+{
+    let text = text.into();
+    let action = IntoObserverSystem::into_system(action);
+    (
+        Name::new("Button"),
+        Node::default(),
+        Node {
+            width: px(BUTTON_WIDTH),
+            height: px(BUTTON_HEIGHT),
+            justify_content: JustifyContent::Center,
+            align_items: AlignItems::Center,
+            ..default()
+        },
+        Children::spawn(SpawnWith(|parent: &mut ChildSpawner| {
+            parent
+                .spawn((
+                    Name::new("Button Inner"),
+                    Button,
+                    BackgroundColor(NODE_BACKGROUND),
+                    InteractionPalette {
+                        none: NODE_BACKGROUND,
+                        hovered: BUTTON_HOVERED_BACKGROUND,
+                        pressed: BUTTON_PRESSED_BACKGROUND,
                     },
-                ),
-            ));
-        });
-
-        entity
-    }
-
-    fn header(&mut self, text: impl Into<String>) -> EntityCommands {
-        let mut entity = self.spawn((
-            Name::new("Header"),
-            NodeBundle {
-                style: Style {
-                    width: Px(500.0),
-                    height: Px(65.0),
-                    justify_content: JustifyContent::Center,
-                    align_items: AlignItems::Center,
-                    ..default()
-                },
-                background_color: BackgroundColor(NODE_BACKGROUND),
-                ..default()
-            },
-        ));
-        entity.with_children(|children| {
-            children.spawn((
-                Name::new("Header Text"),
-                Text::from_section(
-                    text,
-                    TextStyle {
-                        font_size: 40.0,
-                        color: HEADER_TEXT,
-                        ..default()
-                    },
-                ),
-            ));
-        });
-        entity
-    }
-
-    fn label(&mut self, text: impl Into<String>) -> EntityCommands {
-        let mut entity = self.spawn((
-            Name::new("Label"),
-            NodeBundle {
-                style: Style {
-                    width: Px(500.0),
-                    justify_content: JustifyContent::Center,
-                    align_items: AlignItems::Center,
-                    ..default()
-                },
-                ..default()
-            },
-        ));
-        entity.with_children(|children| {
-            children.spawn((
-                Name::new("Label Text"),
-                Text::from_section(
-                    text,
-                    TextStyle {
-                        font_size: 24.0,
-                        color: LABEL_TEXT,
-                        ..default()
-                    },
-                ),
-            ));
-        });
-        entity
-    }
-}
-
-/// An extension trait for spawning UI containers.
-pub trait Containers {
-    /// Spawns a root node that covers the full screen
-    /// and centers its content horizontally and vertically.
-    fn ui_root(&mut self) -> EntityCommands;
-}
-
-impl Containers for Commands<'_, '_> {
-    fn ui_root(&mut self) -> EntityCommands {
-        self.spawn((
-            Name::new("UI Root"),
-            NodeBundle {
-                style: Style {
-                    width: Percent(100.0),
-                    height: Percent(100.0),
-                    justify_content: JustifyContent::Center,
-                    align_items: AlignItems::Center,
-                    flex_direction: FlexDirection::Column,
-                    row_gap: Px(10.0),
-                    position_type: PositionType::Absolute,
-                    ..default()
-                },
-                ..default()
-            },
-        ))
-    }
-}
-
-/// An internal trait for types that can spawn entities.
-/// This is here so that [`Widgets`] can be implemented on all types that
-/// are able to spawn entities.
-/// Ideally, this trait should be [part of Bevy itself](https://github.com/bevyengine/bevy/issues/14231).
-trait Spawn {
-    fn spawn<B: Bundle>(&mut self, bundle: B) -> EntityCommands;
-}
-
-impl Spawn for Commands<'_, '_> {
-    fn spawn<B: Bundle>(&mut self, bundle: B) -> EntityCommands {
-        self.spawn(bundle)
-    }
-}
-
-impl Spawn for ChildBuilder<'_> {
-    fn spawn<B: Bundle>(&mut self, bundle: B) -> EntityCommands {
-        self.spawn(bundle)
-    }
+                    children![(
+                        Name::new("Button Text"),
+                        Text(text),
+                        TextFont::from_font_size(40.0),
+                        TextColor(BUTTON_TEXT),
+                        // Don't bubble picking events from the text up to the button.
+                        Pickable::IGNORE,
+                    )],
+                ))
+                .insert(button_bundle)
+                .observe(action);
+        })),
+    )
 }
